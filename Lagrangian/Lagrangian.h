4eae16686ad7e38fe4e90044684c813ae871189f
Rob Rix <rob.rix@me.com>
2012-11-05 22:04:51 -0500

    Initial Commit

---
+++ Lagrangian/Lagrangian.h
@@ -0,0 +1,13 @@
+//
+//  Lagrangian.h
+//  Lagrangian
+//
+//  Created by Rob Rix on 2012-11-05.
+//  Copyright (c) 2012 Rob Rix. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Lagrangian : NSObject
+
+@end
---
+++ Lagrangian/Lagrangian.m
@@ -0,0 +1,13 @@
+//
+//  Lagrangian.m
+//  Lagrangian
+//
+//  Created by Rob Rix on 2012-11-05.
+//  Copyright (c) 2012 Rob Rix. All rights reserved.
+//
+
+#import "Lagrangian.h"
+
+@implementation Lagrangian
+
+@end
