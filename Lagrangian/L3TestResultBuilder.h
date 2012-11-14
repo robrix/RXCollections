76c7fe7e3868cf2dfc1f27416107dd2d9d406e39
Rob Rix <rob.rix@me.com>
2012-11-14 02:22:35 -0500

    Test result builders feed back more data about changing test results.

--- Lagrangian/L3TestResultBuilder.h
+++ Lagrangian/L3TestResultBuilder.h
@@ -4,6 +4,7 @@
 
 #import "L3EventObserver.h"
 
+@class L3AssertionReference;
 @class L3TestResult;
 @protocol L3TestResultBuilderDelegate;
 
@@ -15,6 +16,9 @@
 
 @protocol L3TestResultBuilderDelegate <NSObject>
 
--(void)testResultBuilder:(L3TestResultBuilder *)builder didCompleteTestResult:(L3TestResult *)result;
+-(void)testResultBuilder:(L3TestResultBuilder *)builder testResultDidStart:(L3TestResult *)result;
+-(void)testResultBuilder:(L3TestResultBuilder *)builder testResult:(L3TestResult *)result didChangeWithSuccessfulAssertionReference:(L3AssertionReference *)assertionReference;
+-(void)testResultBuilder:(L3TestResultBuilder *)builder testResult:(L3TestResult *)result didChangeWithFailedAssertionReference:(L3AssertionReference *)assertionReference;
+-(void)testResultBuilder:(L3TestResultBuilder *)builder testResultDidFinish:(L3TestResult *)result;
 
 @end
