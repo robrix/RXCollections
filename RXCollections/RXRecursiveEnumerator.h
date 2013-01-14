8f0f89bb604c203080b0320ed0552225bf46bfb6
Rob Rix <rob.rix@me.com>
2013-01-14 00:32:11 -0500

    Documents RXRecursiveEnumerator.

--- RXCollections/RXRecursiveEnumerator.h
+++ RXCollections/RXRecursiveEnumerator.h
@@ -4,6 +4,19 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ RXRecursiveEnumerator
+ 
+ Provides NSFastEnumeration (and thus for (… in …) support) for trees whose children are provided under a KVC-compliant selector.
+ 
+ For example:
+ 
+	NSView *view = …; // assumed to exist
+	for (NSView *view in [RXRecursiveEnumerator enumeratorWithTarget:view keyPath:@"subviews"]) { … }
+ 
+ This enumeration is all-inclusive—the root and all branches are mapped as well as any leaves. Heterogeneous branch/leaf trees (i.e. where leaves are not simply branches with zero children) are supported so long as the branches respond to the selector represented by keyPath and the leaves do not (or return an empty collection for that key path).
+ */
+
 @interface RXRecursiveEnumerator : NSObject <NSFastEnumeration>
 
 +(instancetype)enumeratorWithTarget:(id)target keyPath:(NSString *)keyPath;
