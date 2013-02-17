855d95717dbdd5eaaac828cdc2d1564f24b5d20e
Rob Rix <rob.rix@me.com>
2013-02-16 19:42:33 -0500

    RXMappingTraversal is idempotent.
    This is done by autoreleasing the temporaries—the data containing the inner state, and the individual mapped objects—and allowing the caller’s enclosing autorelease pool to manage their lifetimes.
    
    Caveats apply regarding well-formed handling of those external autorelease pools with respect to the calls to -countByEnumeratingWithState:objects:count:; these are documented in RXMappingTraversal.h.
    
    This commit also makes meaningful use of the state’s mutations pointer, ensuring that it is set to the value provided by the internal enumeration. Thus, for(in) will detect mutations to the interior collection during enumeration of it.

--- RXCollections/RXMappingTraversal.h
+++ RXCollections/RXMappingTraversal.h
@@ -4,16 +4,18 @@
 
 #import <RXCollections/RXTraversal.h>
 
-// not thread safe for NSFastEnumeration. just don't.
-
 /**
  RXMappingTraversal
  
  Given an NSFastEnumeration-compliant collection or other enumeration, RXMappingTraversal provides a traversal (and therefore for(in) support) which maps the traversed objects using the supplied block.
  
- This is intended to be used via its opaque interface, RXLazyMap.
+ It is expected to be used via its opaque interface, RXLazyMap, but can be used directly if the need arises.
  
  The block which maps individual objects can safely return nil.
+ 
+ It is safe to enumerate these traversals reentrantly and from multiple threads, so long as each passes a different NSFastEnumeratonState to the traversal.
+ 
+ It is not safe to drain the autorelease pool that -countByEnumeratingWithState:objects:count: is called within between successive calls to it using the same NSFastEnumerationState, as RXMappingTraversal autoreleases objects to ensure correct memory management. Draining an external autorelease pool during the body of a loop is considered bad practice under MRR, and is specifically disallowed under ARC. Plain for(in) will work under MRR or ARC. Creating and draining an autorelease pool in the body of a for(in) loop, as with the `@autoreleasepool` syntax, is safe.
  */
 
 @interface RXMappingTraversal : NSObject <RXTraversal>
