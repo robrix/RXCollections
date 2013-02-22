35109ddaf91b34bfb6a48b45a49265c4325af4f6
Rob Rix <rob.rix@me.com>
2013-02-21 22:19:24 -0500

    Adds a top-level inclusion header.

---
+++ RXCollections/RXCollections.h
@@ -0,0 +1,27 @@
+//  RXCollections.h
+//  Created by Rob Rix on 2013-02-21.
+//  Copyright (c) 2013 Rob Rix. All rights reserved.
+
+#pragma mark Traversals
+
+#import <RXCollections/RXTraversal.h>
+#import <RXCollections/RXEnumerationTraversal.h>
+
+#import <RXCollections/RXRecursiveEnumerator.h>
+
+
+#pragma mark Higher-order functions
+
+#import <RXCollections/RXFilter.h>
+#import <RXCollections/RXFold.h>
+#import <RXCollections/RXMap.h>
+
+
+#pragma mark Pairs
+
+#import <RXCollections/RXPair.h>
+
+
+#pragma mark Collection
+
+#import <RXCollections/RXCollection.h>
