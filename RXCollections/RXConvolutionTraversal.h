c5dcf98a834c38edfe4ba33552245596e35f2fe9
Rob Rix <rob.rix@me.com>
2013-03-30 03:30:38 -0400

    Implements convolution.
    There are two interfaces to convolution, one producing tuples, and the other producing values provided by a block to which it passes the count and a buffer containing the corresponding elements of each sequence.

--- RXCollections/RXConvolutionTraversal.h
+++ RXCollections/RXConvolutionTraversal.h
@@ -7,9 +7,12 @@
 typedef id(^RXConvolutionBlock)(NSUInteger count, id const objects[count]);
 
 /**
- id<RXTraversal> RXConvolveWith(id<RXTraversal> sequences, id(^)(...))
+ id<RXTraversal> RXConvolveWith(id<RXTraversal> sequences, RXConvolutionBlock block)
+ 
+ Traverses the elements of the sequences in lockstep, producing the result of the block (called with the count and an array of the corresponding elements of each sequence) for each one.
+ 
+ RXZipWith is a synonym for this function.
  */
-
 extern id<RXTraversal> RXConvolveWith(id<RXTraversal> sequences, RXConvolutionBlock block);
 extern id (* const RXZipWith)(id<RXTraversal>, RXConvolutionBlock);
 
@@ -29,5 +32,6 @@ extern id (* const RXZip)(id<RXTraversal>);
 +(instancetype)traversalWithSequences:(id<RXTraversal>)sequences block:(RXConvolutionBlock)block;
 
 @property (nonatomic, readonly) id<RXTraversal> sequences;
+@property (nonatomic, copy, readonly) RXConvolutionBlock block;
 
 @end
