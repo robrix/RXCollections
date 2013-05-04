747e52abd10da39bcb8b70f8d7c2d93d08075004
Rob Rix <rob.rix@me.com>
2013-05-03 22:17:39 -0400

    Adds a traversable tree node protocol.

---
+++ RXCollections/RXTree.h
@@ -0,0 +1,11 @@
+//  RXTree.h
+//  Created by Rob Rix on 2013-05-03.
+//  Copyright (c) 2013 Rob Rix. All rights reserved.
+
+#import <RXCollections/RXTraversal.h>
+
+@protocol RXTreeNode <NSObject>
+
+-(id<RXTraversal>)depthFirstTraversal;
+
+@end
