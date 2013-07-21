8e07bc56727b3355c4195e0b657286909c55bc4e
Rob Rix <rob.rix@me.com>
2013-07-21 12:45:20 -0400

    Adds simple, self-contained nil-safe arrays.

---
+++ RXCollections/RXNilArray.m
@@ -0,0 +1,58 @@
+//  RXNilArray.m
+//  Created by Rob Rix on 7/21/2013.
+//  Copyright (c) 2013 Rob Rix. All rights reserved.
+
+#import "RXAllocation.h"
+#import "RXNilArray.h"
+#import "RXSingleton.h"
+
+#import <Lagrangian/Lagrangian.h>
+
+@l3_suite("RXNilArray");
+
+@implementation RXNilArray
+
++(instancetype)allocWithZone:(NSZone *)zone {
+	return RXSingleton(self, ^{ return [self allocateWithExtraSize:0]; });
+}
+
+
+@l3_test("can contain non-nil entries") {
+	NSArray *array = [[RXNilArray alloc] initWithObjects:(const id[]){ test } count:1];
+	l3_assert(array[0], test);
+}
+
+@l3_test("can contain nil entries") {
+	NSArray *array = [[RXNilArray alloc] initWithObjects:(const id [1]){ nil } count:1];
+	l3_assert(array[0], nil);
+}
+
+-(instancetype)initWithObjects:(const id [])objects count:(NSUInteger)count {
+	NSParameterAssert(objects != NULL);
+	if ((self = [[self.class allocateWithExtraSize:sizeof(*objects) * count] init])) {
+		_count = count;
+		id __strong *contents = (id __strong *)self.extraSpace;
+		for (NSUInteger i = 0; i < count; i++) {
+			contents[i] = objects[i];
+		}
+	}
+	return self;
+}
+
+
+#pragma mark NSArray
+
+-(id)objectAtIndex:(NSUInteger)index {
+	NSParameterAssert(index < _count);
+	
+	return ((id __strong *)self.extraSpace)[index];
+}
+
+
+#pragma mark NSCopying
+
+-(instancetype)copyWithZone:(NSZone *)zone {
+	return self;
+}
+
+@end
