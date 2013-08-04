fbb8421492c3be2aff19dcdb1fdbc939bc7139ff
Rob Rix <rob.rix@me.com>
2013-08-04 12:42:07 -0400

    Documents L3SourceReferenceCreate.

--- Lagrangian/L3SourceReference.h
+++ Lagrangian/L3SourceReference.h
@@ -26,6 +26,16 @@
 #define l3_source_reference(...) \
 	L3SourceReferenceCreate(@(__COUNTER__), @(__FILE__), __LINE__, @(#__VA_ARGS__), L3Box(__VA_ARGS__))
 
+/**
+ Creates a reference to a given source location, optionally including the subject of the reference and its source.
+ 
+ @param identifier Delete me.
+ @param file The file being referred to, most often @(__FILE__).
+ @param line The line being referred to, most often __LINE__.
+ @param subjectSource The source code of the subject of the reference. Can be nil.
+ @param subject The subject of the reference. Can be nil.
+ */
+
 L3_EXTERN id<L3SourceReference> L3SourceReferenceCreate(id identifier, NSString *file, NSUInteger line, NSString *subjectSource, id subject);
 
 #endif // L3_SOURCE_REFERENCE_H
