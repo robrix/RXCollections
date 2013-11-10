26f6874195f13fa9ad8c1864b2ba06d1d4790605
Rob Rix <rob.rix@github.com>
2013-11-10 16:14:26 -0500

    Adds enumerators to the framework.
    These are a (slight) generalization of traversals, designed to be
    implementable with NSEnumerator, and based loosely on the description
    of the hierarchy of Range abstractions given by Andrei Alexandrescu[1].
    
    The selection of “enumerator” as a name is intended to evoke
    NSEnumerator.
    
    1: http://www.informit.com/articles/printerfriendly.aspx?p=1407357

---
+++ RXCollections/RXEnumerator.h
@@ -0,0 +1,64 @@
+//  Copyright (c) 2013 Rob Rix. All rights reserved.
+
+@import Foundation;
+
+/**
+ A copyable enumerator.
+ */
+@protocol RXEnumerator <NSObject, NSCopying, NSFastEnumeration>
+
+@property (nonatomic, readonly) bool isExhausted;
+@property (nonatomic, readonly) id currentObject;
+-(void)advanceCurrentObject;
+
+@end
+
+
+/**
+ An enumerator which has a known length.
+ */
+@protocol RXFiniteEnumerator <RXEnumerator>
+
+@property (nonatomic, readonly) NSUInteger length;
+
+@end
+
+
+/**
+ An enumerator which does not have a known length.
+ */
+@protocol RXInfiniteEnumerator <RXEnumerator>
+@end
+
+
+
+/**
+ An enumerator which can be read from at indices.
+ */
+@protocol RXIndexedEnumerator <RXEnumerator>
+
+-(id)objectAtIndexedSubscript:(NSUInteger)index;
+
+-(instancetype)subrangeFromIndex:(NSUInteger)from toIndex:(NSUInteger)to;
+
+@end
+
+
+/**
+ An enumerator which can be written to.
+ */
+@protocol RXOutputEnumerator <RXEnumerator>
+
+@property (nonatomic) id currentObject;
+
+@end
+
+
+/**
+ An enumerator which can be written to at arbitrary indices.
+ */
+@protocol RXIndexedOutputEnumerator <RXIndexedEnumerator, RXOutputEnumerator>
+
+-(void)setObject:(id)object atIndexedSubscript:(NSUInteger)index;
+
+@end
